36b2ab10d7400b79e555170672332855863f250f
Artem Lytvyn <122204149+letv1nnn@users.noreply.github.com>
2025-05-20 20:11:06 +0100

    Update firstTriangle.cpp

--- OpenGL Studying/firstTriangle.cpp
+++ OpenGL Studying/firstTriangle.cpp
@@ -1,12 +1,17 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
+
 
 int main(void)
 {
     GLFWwindow* window;
 
+    /* Initialize the library */
     if (!glfwInit())
         return -1;
 
+    /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
     if (!window)
     {
@@ -14,10 +19,19 @@ int main(void)
         return -1;
     }
     
+    /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+
+    if (glewInit() != GLEW_OK) {
+        std::cout << "Error: GLEW initializing is not OK... exiting\n";
+        exit(1);
+    }
+
+    /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
+        /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
         glBegin(GL_TRIANGLES);
@@ -26,8 +40,10 @@ int main(void)
         glVertex2f(0.5f, -0.25f);
         glEnd();
 
+        /* Swap front and back buffers */
         glfwSwapBuffers(window);
 
+        /* Poll for and process events */
         glfwPollEvents();
     }
 
